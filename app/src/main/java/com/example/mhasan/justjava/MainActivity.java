293f174f86fd6eab758f3b92c99e3ad798a6ad50
Maali Hasan <mhasan@asaltech.com>
2017-05-17 09:59:48 +0300

    first lesson is finished

---
+++ app/src/main/java/com/example/mhasan/justjava/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.mhasan.justjava;
+
+import android.icu.text.NumberFormat;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+    public TextView quantityTextView, priceTextView;
+    public int numberOfCoffees;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    public void init() {
+        quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        priceTextView = (TextView) findViewById(R.id.price_text_view);
+        numberOfCoffees = Integer.valueOf(quantityTextView.getText().toString());
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void submitOrder(View view) {
+        display(numberOfCoffees);
+        displayPrice(numberOfCoffees * 5);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        numberOfCoffees=number;
+        quantityTextView.setText(String.valueOf(numberOfCoffees));
+
+    }
+
+    /**
+     * This method display the given price value on the screen
+     */
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void displayPrice(int number) {
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    public void increment(View view) {
+        int value = Integer.valueOf(quantityTextView.getText().toString());
+        display(value + 1);
+    }
+
+    public void decrement(View view) {
+        int value = Integer.valueOf(quantityTextView.getText().toString());
+        display(value - 1);
+    }
+}
